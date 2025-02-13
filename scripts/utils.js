ce10e6cfb9f73886b7d813f9f225c9af146becda
Yasuo Higa <higayasuo@gmail.com>
2025-02-13 22:29:26 +0900

    Change current directory

--- scripts/utils.js
+++ scripts/utils.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function getPackageName() {
-  const cargoPath = path.join(__dirname, '..', 'Cargo.toml');
+  const cargoPath = path.join(__dirname, '../..', 'Cargo.toml');
   const cargoContent = fs.readFileSync(cargoPath, 'utf8');
   const cargoData = toml.parse(cargoContent);
   return cargoData.package.name;
