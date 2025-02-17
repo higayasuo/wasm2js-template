5e5584443cc64d8cd3b77d33fc83804a06fbf0f0
Yasuo Higa <higayasuo@gmail.com>
2025-02-17 13:16:43 +0900

    Remove original .wasm file after conversion to JavaScript

--- scripts/wasm2js.js
+++ scripts/wasm2js.js
@@ -8,6 +8,9 @@ const pkgDir = 'pkg';
 // Convert wasm to js
 execSync(`wasm2js ${pkgDir}/${packageName}_bg.wasm -o ${pkgDir}/${packageName}_bg.wasm.js`);
 
+// Delete the wasm file as it's no longer needed
+fs.unlinkSync(`${pkgDir}/${packageName}_bg.wasm`);
+
 // Replace wasm import with js import
 const jsFile = `${pkgDir}/${packageName}.js`;
 const content = fs.readFileSync(jsFile, 'utf8')
